01508d6c693bf34799ae1d72a90aef62fbbd3a90
nuggetsnetwork <64025145+nuggetsnetwork@users.noreply.github.com>
2021-01-18 17:40:50 +0530

    bug fix

--- Day15/coffeemachinefinal.py
+++ Day15/coffeemachinefinal.py
@@ -74,7 +74,7 @@ def make_coffee(drink_name, order_ingredients):
 is_on = True
 
 while is_on:
-    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
     if choice == "off":
         is_on = False
     elif choice == "report":
